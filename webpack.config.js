bb0c3be62c5f3b7ef90cf02a8198826871ca1349
Damien L <damien.laneelle33@gmail.com>
2023-08-07 18:50:01 +0200

    add navbar

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,10 @@ Encore
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
   .addEntry("app", "./assets/app.js")
+
   .addEntry("styles", "./assets/styles/styles.scss")
   .addEntry("main_page_styles", "./assets/styles/main_page.scss")
+  .addEntry("navbar_styles", "./assets/styles/navbar.scss")
 
   // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
